ced57bbe69fd6bd2379d3ac33e6b0303c010abf3
cjdelacruz <cjdelacruz0521@gmail.com>
2024-09-07 15:39:30 +0700

    initial commit with app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+console.log('Hello World');
+let name = "Cassandra Jade H. Dela CRuz";
+console.log('My name is' + name);
+
+// .Variable
+Console.log("Variables");
+let name_first = "Mikasa";
+console.log(name_first);
+
+let fruit = "strawberry";
+console.log(fruit);
+
+let weather = "foggy";
+console.log(weather);
+
