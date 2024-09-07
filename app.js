a740c5d6f0bfc91ff23e206ee744f516fe96ce2f
cjdelacruz <cjdelacruz0521@gmail.com>
2024-09-08 00:30:20 +0700

    Initial commit with app.js

--- app.js
+++ app.js
@@ -13,3 +13,39 @@ console.log(fruit);
 let weather = "foggy";
 console.log(weather);
 
+//Numbers
+const j1 = 19
+const j2 = 21
+
+const sum = j1 + j2;
+console.log("The sum of", j1, "and", "j2", "is", sum);
+
+//Booleans
+let c = true;
+console.log(" do you think mosquito can fly?");
+
+let d = false;
+console.log("do you think all flowers are blue");
+
+let x = 19;
+let y = 21;
+
+//Use comparisona operators and store the results in variables
+let isXGreaterThanY = x > y;
+let isXLessThanY = x < y;
+let isXEqualToY = x === y;
+let isXNotEqualToY = x !== y;
+
+console.log("Is X greater than Y?", isXGreaterThanY);
+console.log("Is X less than Y?", isXLessThanY);
+console.log("Is X equal than Y?", isXEqualToY);
+console.log("Is X no equal to Y?", isXNotEqualToY);
+ 
+//strings
+
+console.log("Strings");
+let firstname = "Jade";
+let age = "19";
+let school = "ISPSC";
+
+console.log("Hi! My name is ", + firstname + ", I am ", +age + ", and I am currently studying at ");
